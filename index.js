ac165f44758dc00b3cd526a905991a095b4d2401
SyedMuhgisHussain <syedmughis4541@gmail.com>
2023-12-03 22:13:43 +0500

    Added search functionality

--- index.js
+++ index.js
@@ -1,44 +1,36 @@
 const search_btn = document.querySelector(".search-image");
 const input_Value = document.querySelector(".input");
+const temperature = document.querySelector(".temp");
+const city_Name = document.querySelector(".city-name");
+const humidity_perc = document.querySelector(".humidity-perc");
+const wind_speed = document.querySelector(".wind-speed");
 
-function fetchWeather() {
+
+function main() {
   if (input_Value.value === "") {
     alert("Enter the correct city name.");
   } else {
-    displayWeather(input_Value.value);
+    fetchApi(input_Value.value);
   }
 }
 
-async function displayWeather(cityName) {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=ed9762be5575457144a931c00af77267`;
+async function fetchApi(cityName) {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=ed9762be5575457144a931c00af77267&units=metric`;
     const response = await fetch(url);
     const data = await response.json();
-    console.log(data);
-  const serchBar = document.querySelector(".serchBar");
-  const newEle = document.createElement("div");
-  newEle.classList.add("all-data");
-  newEle.innerHTML = `
-    <img src="./images/clear.png" class="image-weather" alt="random-image">
-                <p class="temp">7°c</p>
-                <p class="city-name">Karachi</p>
-                <div class="last">
-                    <div class="left">
-                        <img src="./images/humidity.png" class="humidity-image" alt="humidity">
-                        <p class="humidity-perc">48%</p>
-                    </div>
-                    <div class="right">
-                        <img src="./images/wind.png" class="wind-image" alt="Wind">
-                        <p class="wind-speed">10.29 km/h</p>
-                    </div>
-                </div>
-                <div class="text">
-                    <p>Humidity</p>
-                    <p>Wind Speed</p>
-                </div>
-    `;
-    serchBar.after(newEle);
-    input_Value.value = '';
-
+    showInFrontEnd(data);
 }
 
-search_btn.addEventListener("click", fetchWeather);
+function showInFrontEnd(weather) {
+    temperature.textContent = `${weather.main.temp}°c`;
+    city_Name.textContent = `${weather.name}`;
+    humidity_perc.textContent = `${weather.main.humidity}%`;
+    wind_speed.textContent = `${weather.wind.speed}km/h`;
+    input_Value.value = '';
+}
+
+window.addEventListener('load', function() {
+    fetchApi('karachi');
+});
+
+search_btn.addEventListener("click", main);
